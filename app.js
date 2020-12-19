8a6f3d7db19dcf1c57e26fb0d1e33c4715a4d40e
PhamPhiL <thaiphilong.pham@gmail.com>
2020-12-19 03:39:17 +0100

    delete token

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const prefix = config.prefix;
+const prefix = config2.prefix;
 
-client.login(config.token); 
+client.login(config2.token); 
 
  
 client.on('ready', () => {
